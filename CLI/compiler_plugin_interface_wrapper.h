001de30fe53d5370d53c6463792d5d049cef1fdb
tcs <tsvi.cherny-shahar@bluevoyant.com>
2020-06-17 19:12:42 +0300

    add compiler example

--- CLI/compiler_plugin_interface_wrapper.h
+++ CLI/compiler_plugin_interface_wrapper.h
@@ -15,12 +15,12 @@ public:
 	explicit compiler_plugin_interface_wrapper(const std::string& plugin_filename);
 
 	/**
-	 * Load runtime runtime of foreign runtime
+	 * Compile IDL to code called from XLLR to the foreign function
 	 */ 
 	void compile_to_guest(const char* idl_path, uint32_t idl_path_length, const char* output_path, uint32_t output_path_length, char** out_err, uint32_t* out_err_len) override;
 
 	/**
-	 * Free runtime runtime of foreign runtime
+	 * Compile IDL to code calling to XLLR from host code
 	 */ 
 	void compile_from_host(const char* idl_path, uint32_t idl_path_length, const char* output_path, uint32_t output_path_length, char** out_err, uint32_t* out_err_len) override;
 
