51f8a12f3c8489c320a42131ed5be82ae3b34902
tcs <tsvi.cherny-shahar@bluevoyant.com>
2020-08-11 08:00:28 +0300

    go2python WIP

---
+++ examples/go2python/PythonicUtils.py
@@ -0,0 +1,10 @@
+
+def hello_world():
+	print('Hello World - From Python!')
+
+def div(x, y):
+	return x / y
+
+def concat_strings(arr_of_strings):
+	res = ', '.join(arr_of_strings)
+	return res
---
+++ examples/go2python/run_example.bat
@@ -0,0 +1,18 @@
+@echo off
+
+rem compile protobuf
+echo Generating PythonFuncs_pb2.py by compiling PythonFuncs.proto with protoc
+protoc --python_out=. PythonFuncs.proto
+
+rem compile openffi
+echo Generating PythonFuncs_openffi_guest.py and PythonFuncs_openffi_host.py by compiling PythonFuncs.proto with openffi
+openffi -c --idl PythonFuncs.proto -f python -t
+
+rem run examples
+echo Running Example Functions
+python run.py
+
+rem if there's an unexpected error - print there's an error!
+if %ERRORLEVEL% GEQ 1 echo Example Failed && exit /b 1
+
+echo ==== Examples executed successfully ====
