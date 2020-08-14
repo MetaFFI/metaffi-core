de2a6e8ea46ee9c9677054b22978fc27c67a8f3f
tcs <tsvi.cherny-shahar@bluevoyant.com>
2020-08-14 14:20:43 +0300

    run all examples

---
+++ examples/run_all_examples.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo 'Running Go->Go'
+cd go
+./run_example.sh 
+cd ..
+echo =======================================
+echo 'Running Python3->Python3'
+cd python3
+./run_example.sh
+cd ..
+echo =======================================
+echo 'Running Go->Python3'
+cd go2python3
+./run_example.sh
+cd ..
+echo =======================================
+echo 'Running Python3->Go'
+cd python32go
+./run_example.sh
+cd ..
+echo =======================================
+eco 'Running Go->Python3'
+cd go2python3
+./run_example.sh
+cd ..
+echo =======================================
+echo 'Running Real World Go->Python3'
+cd RWgo2python3
+./run_example.sh
+cd ..
+echo =======================================
+echo 'Running Real World Python3->Go'
+cd RWpython32go
+./run_example.sh
+cd ..
+echo =======================================
+echo Done!
+
