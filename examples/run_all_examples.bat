ce2c3635fe7cf237f2d4e5b3ca22b2d69f31dbe3
Unknown <tsvi.cherny-shahar@bluevoyant.com>
2020-08-23 10:31:17 +0300

    fix echo to use double quotes

--- examples/run_all_examples.bat
+++ examples/run_all_examples.bat
@@ -1,31 +1,31 @@
 @echo off
 
-echo 'Skipping Go->Go example (experimental. Go cannot load a Go C-Shared library)'
+echo "Skipping Go->Go example (experimental. Go cannot load a Go C-Shared library)"
 rem cd go
 rem run_example.bat
 rem cd ..
 echo =======================================
-echo 'Running Python3->Python3'
+echo "Running Python3->Python3"
 cd python3
 run_example.bat
 cd ..
 echo =======================================
-echo 'Running Go->Python3'
+echo "Running Go->Python3"
 cd go2python3
 run_example.bat
 cd ..
 echo =======================================
-echo 'Running Python3->Go'
+echo "Running Python3->Go"
 cd python32go
 run_example.bat
 cd ..
 echo =======================================
-echo 'Running Real World Go->Python3'
+echo "Running Real World Go->Python3"
 cd RWgo2python3
 run_example.bat
 cd ..
 echo =======================================
-echo 'Running Real World Python3->Go'
+echo "Running Real World Python3->Go"
 cd RWpython32go
 run_example.bat
 cd ..
