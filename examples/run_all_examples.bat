b882af21a9e4d8c3419a2fec775f526344c9761a
Unknown <tsvi.cherny-shahar@bluevoyant.com>
2020-08-25 07:45:48 +0300

    use call command to run examples

--- examples/run_all_examples.bat
+++ examples/run_all_examples.bat
@@ -2,32 +2,32 @@
 
 echo "Skipping Go->Go example (experimental. Go cannot load a Go C-Shared library)"
 rem cd go
-rem run_example.bat
+rem call run_example.bat
 rem cd ..
 echo =======================================
 echo "Running Python3->Python3"
 cd python3
-run_example.bat
+call run_example.bat
 cd ..
 echo =======================================
 echo "Running Go->Python3"
 cd go2python3
-run_example.bat
+call run_example.bat
 cd ..
 echo =======================================
 echo "Running Python3->Go"
 cd python32go
-run_example.bat
+call run_example.bat
 cd ..
 echo =======================================
 echo "Running Real World Go->Python3"
 cd RWgo2python3
-run_example.bat
+call run_example.bat
 cd ..
 echo =======================================
 echo "Running Real World Python3->Go"
 cd RWpython32go
-run_example.bat
+call run_example.bat
 cd ..
 echo =======================================
 echo Done!
