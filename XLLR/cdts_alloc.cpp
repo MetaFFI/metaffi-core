61fad32ca2514e1949c15e15ca789232b2c189b8
GreenFuze <6296195+GreenFuze@users.noreply.github.com>
2024-04-29 15:42:57 +0300

    handling CDTS on cache + adding xllr_capi_test.cpp

--- XLLR/cdts_alloc.cpp
+++ XLLR/cdts_alloc.cpp
@@ -5,6 +5,16 @@
 thread_local cdt cdt_cache[cdts_cache_size];
 thread_local cdts cdts_cache[2];
 
+struct initialize_cdts_cache
+{
+	initialize_cdts_cache()
+	{
+		cdts_cache[0].allocated_on_cache = 1;
+		cdts_cache[1].allocated_on_cache = 1;
+	}
+};
+static initialize_cdts_cache init_cdts_cache;
+
 //--------------------------------------------------------------------
 extern "C" cdts* alloc_cdts_buffer(metaffi_size params_count, metaffi_size ret_count)
 {
@@ -24,8 +34,10 @@ extern "C" cdts* alloc_cdts_buffer(metaffi_size params_count, metaffi_size ret_c
 	{
 		cdts_cache[0].arr = new cdt[params_count+ret_count];
 		cdts_cache[0].length = params_count;
+		cdts_cache[0].allocated_on_cache = 0;
 		cdts_cache[1].arr = cdts_cache[0].arr+params_count;
 		cdts_cache[1].length = ret_count;
+		cdts_cache[1].allocated_on_cache = 0;
 	}
 	
 	return (cdts*)cdts_cache;
